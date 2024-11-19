93af2c7b72df490299f7c46a956172108d050154
Bambam <exp.ai@outlook.com>
2024-11-19 00:09:11 +0000

    errors

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import Upgrade from "./upgrades";
+import Upgrade from "../components/upgrades";
 
 const Controls = ({ buyTopping, buyJarUpgrade }) => {
   return (
     <div className="controls">
-      <Button Onclick={incrementCookies} />
       <Button Onclick={buyUpgrade} />
       <Button Onclick={buyjarUpgrade} />
     </div>
